67cfe6553fe0cf0a89b25b0018f50be66ed6b9dc
Helengendary <helenapicinin13@gmail.com>
2024-08-14 10:29:47 -0300

    desafio terminado

--- java-script/Desafio Js trevisan/2. SiteApple/script.js
+++ java-script/Desafio Js trevisan/2. SiteApple/script.js
@@ -1,15 +1,23 @@
 let temaCor = document.getElementById("nome-cor-selecionada"); 
-let atual = 0;
-let indexFoto = 1;
-let isCheckedCor = false;
-let isCheckedTam = false;
-let pastaCor = "verde-cipreste"
-let tituloCor = "Verde Cipreste"
-let TamanhoImagem = ""
 let principal = document.getElementById("imagem-visualizacao");
 let miniCostas = document.getElementById("0-imagem-miniatura");
 let miniLadin = document.getElementById("1-imagem-miniatura");
 let miniFrente = document.getElementById("2-imagem-miniatura");
+let tituloProduto = document.getElementById("titulo-produto");
+
+
+let atual = 0;
+let indexFoto = 1;
+let tamanhoRelogio = 45
+
+let isCheckedCor = false;
+let isCheckedTam = false;
+let isCheckedSize = false;
+
+
+let pastaCor = "verde-cipreste"
+let tituloCor = "Verde Cipreste"
+let TamanhoImagem = ""
 
 temaCor.innerHTML += tituloCor;
 
@@ -50,6 +58,8 @@ function trocaCor()
             miniCostas.setAttribute("src", "./imagens/opcoes-cores/imagens-" + pastaCor  + "/imagem-0.jpeg");
             miniLadin.setAttribute("src", "./imagens/opcoes-cores/imagens-" + pastaCor  + "/imagem-1.jpeg");
             miniFrente.setAttribute("src", "./imagens/opcoes-cores/imagens-" + pastaCor  + "/imagem-2.jpeg");
+
+            tituloProduto.innerHTML = "Pulseira loop esportiva " + pastaCor + " para caixa de " + tamanhoRelogio + " mm"
         }
     }
 
@@ -62,21 +72,45 @@ function trocarImagem() {
 
         let idImagem = index + "-imagem";
 
-        console.log(idImagem)
         let imagem = document.getElementById(idImagem);
 
         isCheckedTam = imagem.checked;
 
-        console.log(isCheckedTam);
 
         if (isCheckedTam) {
             
             indexFoto = index;
 
-            console.log(isCheckedTam);
             principal.setAttribute("src", "./imagens/opcoes-cores/imagens-" + pastaCor  + "/imagem-"  + index +".jpeg");
         }
     }
 
     isCheckedTam = false;
+}
+
+function trocarTamanho() {
+
+    for (let index = 0; isCheckedSize == false; index++) {
+
+        let idTamanho = index + "-tamanho";
+
+        let size = document.getElementById(idTamanho);
+
+        isCheckedSize = size.checked;
+
+        if (isCheckedSize) {
+
+            if (index == 0) {
+                tamanhoRelogio = 41;
+                principal.setAttribute("style", "width:90%");
+            } else {
+                tamanhoRelogio = 45;
+                principal.setAttribute("style", "width:100%");
+            }
+
+            tituloProduto.innerHTML = "Pulseira loop esportiva " + pastaCor + " para caixa de " + tamanhoRelogio + " mm"
+        }
+    }
+
+    isCheckedSize = false;
 }
